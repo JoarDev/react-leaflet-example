db2b303076ccd6b409c4cc5f19e51c442449988f
JoarDev <j.ramos5452@gmail.com>
2022-10-13 18:04:43 -0300

    add reverse geocoding api

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import { MapContainer } from 'react-leaflet/MapContainer';
 import { TileLayer } from 'react-leaflet/TileLayer';
 import { Marker, Popup, useMapEvents } from 'react-leaflet';
 
-function MyComponent({ location, setLocation }) {
+function MyComponent({ location, setLocation, setLocationName }) {
   useMapEvents({
     click: (e) => {
       console.log('event', e);
       setLocation(e.latlng);
+      const getGeocodingUrl = ({ lat, lng }) =>
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`;
+      fetch(getGeocodingUrl(e.latlng))
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('data', data);
+          setLocationName(data.locality);
+        })
+        .catch((error) => console.error(error));
     },
   });
   return (
@@ -23,6 +32,7 @@ function MyComponent({ location, setLocation }) {
 const center = [52.22977, 21.01178];
 
 function App() {
+  const [locationName, setLocationName] = useState('');
   const [location, setLocation] = useState({
     lat: 12,
     lng: 13,
@@ -30,6 +40,7 @@ function App() {
   return (
     <div className="App">
       <h1>React + Leaflet</h1>
+      <h3>Location name: {locationName}</h3>
       <h3>
         latitude:{location.lat} longitude:{location.lng}
       </h3>
@@ -38,7 +49,11 @@ function App() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <MyComponent location={location} setLocation={setLocation} />
+        <MyComponent
+          location={location}
+          setLocation={setLocation}
+          setLocationName={setLocationName}
+        />
       </MapContainer>
     </div>
   );
